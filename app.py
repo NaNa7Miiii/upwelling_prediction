74d2adcf876e4a8baed2144f65dfb770ffc7f5ab
Jiarui Yu <97627889+NaNa7Miiii@users.noreply.github.com>
2023-08-25 14:40:21 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,148 @@
+import datetime
+
+from utils import *
+
+# Set up session state variables
+session_vars = ['data_fetched', 'step', 'view', 'sst_data', 'ds_list']
+default_values = [False, 0, 'main', [], []]
+for var, default in zip(session_vars, default_values):
+    st.session_state.setdefault(var, default)
+
+# Load model
+model = load_your_model('ConvLSTM_2002_2017.keras')
+
+# App Title and Sidebar
+st.title('🌊 SST Prediction App 🌊')
+intro_placeholder = st.empty()
+
+if not st.session_state.get("data_fetched", False):
+    intro_placeholder.write(INTRO_TEXT)
+
+st.sidebar.title("Options")
+st.sidebar.write(SIDEBAR_DESC)
+default_end_date = datetime.date.today() - datetime.timedelta(days=2)
+default_start_date = default_end_date - datetime.timedelta(days=4)
+start_date = st.sidebar.date_input("Start Date", default_start_date)
+end_date = st.sidebar.date_input("End Date", start_date + datetime.timedelta(days=4))
+start_time = f"{start_date}T21:00:00Z"
+end_time = f"{end_date}T20:59:59Z"
+
+# Fetching Data
+with st.sidebar.expander("Fetching Data"):
+    if st.sidebar.button("Fetch Data"):
+        intro_placeholder.empty()
+        with st.spinner('Fetching Data...'):
+            ds = fetch_mur_data(start_time, end_time, slice(-5, 35), slice(45, 90))
+        if len(ds['time']) == 5:
+            st.session_state.data_fetched = True
+            st.session_state.sst_data = [process_nc(ds.isel(time=i)) for i in range(5)]
+            st.session_state.ds_list = [ds.isel(time=i) for i in range(5)]
+        else:
+            st.warning("Please select a date range with exactly 5 days of available data!")
+
+# Display Data
+for idx, (sst_value, ds) in enumerate(zip(st.session_state.sst_data, st.session_state.ds_list)):
+    if sst_value.all() and ds.all():
+        plot_sst(sst_value, ds, f"Sea Surface Temperature for File #{idx + 1}")
+        if st.button(f"See Details for File #{idx + 1}"):
+            st.session_state.view = 'details'
+            st.session_state.detail_idx = idx
+            st.experimental_rerun()
+
+if st.session_state.view == 'details':
+    sst_value, ds = st.session_state.sst_data[st.session_state.detail_idx], st.session_state.ds_list[st.session_state.detail_idx]
+    if sst_value.all() and ds.all():
+        plot_sst_interactive(sst_value, ds, f"Sea Surface Temperature for File #{st.session_state.detail_idx + 1}")
+        if st.button("Return from Details"):
+            st.session_state.view = 'main'
+            st.experimental_rerun()
+
+# Prediction
+prediction_container = st.empty()
+if st.session_state.data_fetched:
+    with st.spinner('Running Prediction...'):
+        if st.button('🔍 Run Prediction 🔍'):
+            preprocessed_data = [preprocess_vis_input_data(day) for day in st.session_state.sst_data]
+            input_data = np.array(preprocessed_data)[:, ::10, ::10, np.newaxis]
+            prediction = model.predict(input_data[np.newaxis, ...])[0]
+            st.session_state.prediction_postprocessed = postprocess_prediction(prediction, st.session_state.sst_data[-1][::10, ::10])
+            st.session_state.step = 1
+
+# Display Predicted Data Details
+if "prediction_postprocessed" in st.session_state:
+    last_ds = st.session_state.ds_list[-1]
+    plot_sst(st.session_state.prediction_postprocessed, last_ds, "Predicted Sea Surface Temperature")
+    if st.button('See Details for Predicted Data'):
+        st.session_state.view = 'predicted_details'
+        st.experimental_rerun()
+
+if st.session_state.view == 'predicted_details':
+    last_ds = st.session_state.ds_list[-1]
+    plot_sst_interactive_init(st.session_state.prediction_postprocessed, last_ds, "Detailed Predicted Sea Surface Temperature")
+    if st.button("Return from Predicted Details"):
+        st.session_state.view = 'main'
+        st.experimental_rerun()
+
+# True Data Section
+st.session_state.setdefault("true_values", None)
+
+if st.session_state.view == 'true_details' and st.session_state.true_values:
+    last_ds = st.session_state.ds_list[-1]
+    plot_sst_interactive(st.session_state.true_values_fullres, last_ds, "Detailed Real Image")
+    if st.button("Return from True Details"):
+        st.session_state.view = 'main'
+        st.experimental_rerun()
+
+
+
+if st.session_state.step == 1:
+    date_input = st.date_input("Choose a date (The Default Day is The Following Day)", end_date + datetime.timedelta(days=1))
+    if date_input:
+        with st.spinner("Plotting the True Data..."):
+            start_time, end_time = f"{date_input}T09:00:00.000000000", f"{date_input}T09:23:59.000000000"
+            true_data = fetch_mur_data(start_time, end_time, slice(-5.0, 35.0), slice(45.0, 90.0))
+            true_values = np.squeeze(process_nc(true_data))
+            st.session_state.true_values, st.session_state.true_values_fullres = true_values[::10, ::10], true_values
+            plot_sst(true_values, true_data, "Real Image")
+            st.session_state.step = 2
+
+# Evaluation Metrics
+if st.session_state.step == 2:
+    metrics = st.multiselect("Choose metrics for evaluation", ["MSE", "MAE"])
+
+    prediction_postprocessed = st.session_state.prediction_postprocessed
+    true_values = st.session_state.true_values
+    results = {}
+
+    if st.button("See Difference between Predicted and True Data"):
+        st.session_state.view = 'difference_map'
+        st.experimental_rerun()
+
+    if "MSE" in metrics:
+        results["MSE: Prediction Vs True Data"] = np.nanmean((prediction_postprocessed - true_values) ** 2)
+        results["MSE: Last Input Day Vs True Data "] = np.nanmean((st.session_state.sst_data[-1][::10, ::10] - true_values) ** 2)
+
+    if "MAE" in metrics:
+        results["MAE: Prediction Vs True Data"] = np.nanmean(np.abs(prediction_postprocessed - true_values))
+        results["MAE: Last Input Day Vs True Data "] = np.nanmean(np.abs(st.session_state.sst_data[-1][::10, ::10] - true_values))
+
+    for key, val in results.items():
+        st.write(f"{key}: {val:.4f}")
+
+# Difference Map View
+if st.session_state.view == 'difference_map':
+    diff_map = prediction_postprocessed - true_values
+
+    # Find the absolute maximum difference to ensure the color scale is centered at zero
+    max_diff = np.nanmax(np.abs(diff_map))
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(diff_map, cmap='bwr', interpolation='none', origin='lower', vmin=-max_diff, vmax=max_diff)
+    ax.set_title("Difference Map: Predicted - True")
+    fig.colorbar(im, ax=ax)
+    st.pyplot(fig)
+    st.write(DIFFMAP_DESC)
+
+    if st.button("Return from Difference Map"):
+        st.session_state.view = 'main'
+        st.experimental_rerun()
---
+++ utils.py
@@ -0,0 +1,201 @@
+from keras.models import load_model
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import streamlit as st
+import requests
+import xarray as xr
+import os
+
+
+def load_your_model(model_path):
+    model = load_model(model_path)
+    return model
+
+
+def process_nc(ds):
+    sst_values = ds.values
+    sst_values = np.where(sst_values == 9.96921e+36, np.nan, sst_values)
+    return sst_values
+
+
+def preprocess_vis_input_data(day_data):
+    day_data = np.squeeze(day_data)
+    mean_val = np.nanmean(day_data)
+    processed_data = day_data - mean_val
+    # Replace NaNs with 0.0
+    processed_data = np.where(np.isnan(processed_data), 0.0, processed_data)
+    return processed_data
+
+
+def postprocess_prediction(prediction, input_data_day):
+    land_mask = create_land_mask(input_data_day)
+    prediction_squeezed = np.squeeze(prediction)
+    prediction_squeezed[land_mask] = np.nan
+    mean_val = np.nanmean(input_data_day)
+    prediction_post = np.where(np.isnan(prediction_squeezed), np.nan, prediction_squeezed + mean_val)
+    return prediction_post
+
+
+def create_land_mask(data):
+    data_copy = data.copy()
+    data_copy[data_copy == 0] = np.nan
+    return np.isnan(data_copy)
+
+
+def plot_sst(sst_values, ds, title_text):
+    fig, ax = plt.subplots()
+    lons = ds['lon'].values
+    lats = ds['lat'].values
+    extent = [lons.min(), lons.max(), lats.min(), lats.max()]
+    img = ax.imshow(sst_values, origin="lower", cmap="viridis", extent=extent)
+    ax.set_xticks(np.linspace(lons.min(), lons.max(), 6))
+    ax.set_yticks(np.linspace(lats.min(), lats.max(), 6))
+    ax.set_xlabel("Longitude")
+    ax.set_ylabel("Latitude")
+    ax.set_title(title_text)
+    plt.colorbar(img)
+    st.pyplot(fig)
+
+
+def downsample_data(data, step=10):
+    return data[::step, ::step]
+
+
+def plot_sst_interactive(sst_values, ds, title_text):
+    sst_values_downsampled = downsample_data(sst_values)
+    lons = ds['lon'].values[::10]
+    lats = ds['lat'].values[::10]
+    fig = go.Figure(data=go.Heatmap(
+        z=sst_values_downsampled,
+        x=lons,
+        y=lats,
+        colorscale="Viridis",
+        colorbar={"title": "SST Values"}
+    ))
+    fig.update_layout(title=title_text, xaxis_title="Longitude", yaxis_title="Latitude")
+    fig.update_layout(dragmode=False)
+    st.plotly_chart(fig)
+
+
+def plot_sst_interactive_init(sst_values, ds, title_text):
+    lons = ds['lon'].values
+    lats = ds['lat'].values
+    fig = go.Figure(data=go.Heatmap(
+        z=sst_values,
+        x=lons,
+        y=lats,
+        colorscale="Viridis",
+        colorbar={"title": "SST Values"}
+    ))
+    fig.update_layout(title=title_text, xaxis_title="Longitude", yaxis_title="Latitude")
+    fig.update_layout(dragmode=False)
+    st.plotly_chart(fig)
+
+
+def fetch_data_url(start_time, end_time, lats, lons):
+    cmr_url = 'https://cmr.earthdata.nasa.gov/search/granules.json'
+    response = requests.get(cmr_url,
+                            params={
+                                'provider': 'POCLOUD',
+                                'short_name': 'MUR-JPL-L4-GLOB-v4.1',
+                                'temporal': f'{start_time},{end_time}',
+                                'bounding_box': f'{lons.start},{lats.start},{lons.stop},{lats.stop}',
+                                'page_size': 2000,
+                            }
+                            )
+    granules = response.json()['feed']['entry']
+    return [next((link['href'] for link in granule['links'] if link['rel'].endswith('/data#')), None) for granule in
+            granules]
+
+
+def fetch_and_load_sst(url, lats, lons):
+    filename = url.split('/')[-1]
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            print(f"Failed to download from {url}. Status code: {response.status_code}")
+            return None
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        ds = xr.open_dataset(filename)
+        sst_data = ds['analysed_sst'].sel(lat=slice(lats.start, lats.stop), lon=slice(lons.start, lons.stop))
+        os.remove(filename)
+        return sst_data
+    except Exception as e:
+        print(f"Error fetching and loading SST data from {url}: {e}")
+        if os.path.exists(filename):
+            os.remove(filename)
+        return None
+
+
+def fetch_mur_data(start_time, end_time, lats, lons):
+    cmr_url = 'https://cmr.earthdata.nasa.gov/search/granules.json'
+    response = requests.get(cmr_url,
+                            params={
+                                'provider': 'POCLOUD',
+                                'short_name': 'MUR-JPL-L4-GLOB-v4.1',
+                                'temporal': f'{start_time},{end_time}',
+                                'bounding_box': f'{lons.start},{lats.start},{lons.stop},{lats.stop}',
+                                'page_size': 2000,
+                            }
+                            )
+    granules = response.json()['feed']['entry']
+    urls = [next((link['href'] for link in granule['links'] if link['rel'].endswith('/data#')), None) for granule in
+            granules]
+    sst_data_list = [fetch_and_load_sst(url, lats, lons) for url in urls]
+    # Then, concatenate all the sst_data elements from the list
+    resulting_dataset = xr.concat(sst_data_list, dim="time")
+    return resulting_dataset
+
+
+INTRO_TEXT = """
+    Welcome to the SST Prediction App!
+    
+    This application allows users to predict Sea Surface Temperatures (SST) based on historical data.
+    Using a specialized ConvLSTM model, this app ingests 5 days of SST data and predicts the SST for the 6th day.
+    
+    ### Model Overview:
+    
+    Layer (type) | Output Shape | Param #
+    ---|---|---
+    ConvLSTM2D | (None, 401, 451, 32) | 38144
+    BatchNormalization | (None, 401, 451, 32) | 128
+    Dropout | (None, 401, 451, 32) | 0
+    Conv2D | (None, 401, 451, 64) | 18496
+    BatchNormalization_1 | (None, 401, 451, 64) | 256
+    Dropout_1 | (None, 401, 451, 64) | 0
+    Conv2D_1 | (None, 401, 451, 32) | 18464
+    BatchNormalization_2 | (None, 401, 451, 32) | 128
+    Dropout_2 | (None, 401, 451, 32) | 0
+    Conv2D_2 | (None, 401, 451, 1) | 289
+    
+    **Total params:** 75,905
+    **Trainable params:** 75,649
+    **Non-trainable params:** 256
+    
+    The data utilized originates from the [MUR-JPL-L4-GLOB-v4.1](https://search.earthdata.nasa.gov/search/granules?p=C1996881146-POCLOUD&pg[0][v]=f&pg[0][gsk]=-start_date&q=MUR-JPL-L4-GLOB-v4.1&fi=MODIS&as[instrument][0]=MODIS&tl=1686459841!3!!&zoom=0) dataset on earthdata.nasa.gov.
+    
+    **Please fetch the data for 5 days at the left sidebar first.**
+    """
+
+SIDEBAR_DESC = """
+    ### 1. Select Dates
+    
+    The data for this app is sourced from the MUR-JPL-L4-GLOB-v4.1 dataset available at [earthdata.nasa.gov](https://search.earthdata.nasa.gov/search/granules?p=C1996881146-POCLOUD&pg[0][v]=f&pg[0][gsk]=-start_date&q=MUR-JPL-L4-GLOB-v4.1&fi=MODIS&as[instrument][0]=MODIS&tl=1686459841!3!!&zoom=0).
+    
+    By default, the 'end date' is set as yesterday. The range between the start and end date encompasses 5 days of data.
+    """
+
+DIFFMAP_DESC = """
+    ## Difference Map Explanation
+
+    The difference map visualizes the discrepancy between predicted and true values. It's calculated by subtracting the true values from the predicted values. 
+
+    - **Blue regions**: The predicted value is lower than the true value.
+    - **Red regions**: The predicted value is higher than the true value.
+    - **White regions**: The predicted value is very close or equal to the true value.
+
+    Difference maps are valuable in machine learning and data analytics to visualize prediction errors, thereby guiding researchers and developers towards potential areas of improvement.
+    """
+
